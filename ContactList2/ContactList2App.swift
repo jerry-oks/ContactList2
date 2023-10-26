486b4c0f60f8918aab8d751b07780933fb224c3a
Vadim Vanatov <79990006611@yandex.ru>
2023-10-26 12:11:07 +0300

    Initial Commit

---
+++ ContactList2/ContactList2App.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList2App.swift
+//  ContactList2
+//
+//  Created by HOLY NADRUGANTIX on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
